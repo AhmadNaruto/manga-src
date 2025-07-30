5e21ab9c39e75f20486cf36e7e2763b8f66cf8ff
theAngka <47007634+AhmadNaruto@users.noreply.github.com>
2025-07-30 15:29:27 +0800

    Update WuxiaWorld.kt

--- src/en/wuxiaworld/src/eu/kanade/tachiyomi/extension/en/wuxiaworld/WuxiaWorld.kt
+++ src/en/wuxiaworld/src/eu/kanade/tachiyomi/extension/en/wuxiaworld/WuxiaWorld.kt
@@ -3,6 +3,6 @@ package eu.kanade.tachiyomi.extension.en.wuxiaworld
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
 class WuxiaWorld : Madara("WuxiaWorld", "https://wuxiaworld.site", "en") {
-    override val mangaSubString = "novel"
+    override val mangaSubString = "comic"
     override val useNewChapterEndpoint = true
 }
